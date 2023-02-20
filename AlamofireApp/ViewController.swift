78d35f061520d4307da0639ec1c93b84bf24a324
AlexanderGorelkin <betmen2000@inbox.ru>
2023-02-20 14:17:29 +0300

    Revert "Простые примеры использование AF"
    This reverts commit 741d112bc9d931e97d39e4944c90c55ef538ffdd.

--- AlamofireApp/ViewController.swift
+++ AlamofireApp/ViewController.swift
@@ -6,44 +6,14 @@
 //
 
 import UIKit
-import Alamofire
 
 class ViewController: UIViewController {
-    @IBOutlet weak var imageView: UIImageView!
-    @IBOutlet weak var indicator: UIActivityIndicatorView!
-    @IBOutlet weak var label: UILabel!
-    @IBOutlet weak var progressView: UIProgressView!
-    
-    let imageUrl: String = "https://i.stack.imgur.com/cdCSj.jpg"
-    
+
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        
-        indicator.startAnimating()
-        getImage(imageUrl)
-        
+        // Do any additional setup after loading the view.
     }
-    
-    func getImage(_ url: String) {
-        AF.download(url)
-            .validate()
-            .downloadProgress { (progress) in
-                self.label.text = progress.localizedDescription
-                self.progressView.setProgress(Float(progress.fractionCompleted), animated: true)
-            }
-            .responseData { (response) in
-                guard let data = response.value else { return }
-                self.imageView.image = UIImage(data: data)
-                self.indicator.stopAnimating()
-                self.indicator.isHidden = true
-                self.progressView.isHidden = true
-                
-            }
-        
-        
-    }
-    
-    
+
+
 }
 
